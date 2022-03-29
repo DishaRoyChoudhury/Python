cb7fde9db5678da68d13c490ee3f5cd15f511e6a
DishaRoyChoudhury <77383900+DishaRoyChoudhury@users.noreply.github.com>
2022-03-29 17:10:24 +0530

    Add files via upload

---
+++ Senior citizen or not.py
@@ -0,0 +1,5 @@
+age=int(input("Enter person's age:"))
+if(age >=60 ):
+    print("Senior citizen")
+else:
+    print("Not senior citizen")
---
+++ lowest between 2.py
@@ -0,0 +1,7 @@
+num1=int(input("Number 1 : "))
+num2=int(input("Number 2 : "))
+
+if(num1>num2):
+    print("number 2 is lowest")
+else:
+    print("number 1 is lowest")
